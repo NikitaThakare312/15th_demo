1453d19a8e8bbce293c3c2daf6a3f631f2c3fd50
NikitaThakare312 <nikitathakare312@gmail.com>
2023-12-17 19:17:14 +0530

    15th assignment: promises.all using

--- index.js
+++ index.js
@@ -8,31 +8,38 @@ const preMovie=async()=>{
 
     const getPopcorn = new Promise((resolve,reject)=>resolve(`popcorn`));
 
+    const getCandy= new Promise((resolve,reject)=> resolve(`candy`));
+
+    const getCoke = new Promise((resolve,reject)=>resolve(`coke`));
+
+
     let ticket=await promiseWifeBringingTicks;
 
-    const addButter= new Promise((resolve,reject)=>resolve(`butter`));
+    let [popcorn,candy,coke]=await Promise.all([getPopcorn,getCandy,getCoke])
 
-    console.log(`wife:i have ${ticket}`);
-    console.log('husband: we should go in');
-    console.log('wife: no i am hungry');
+    console.log(`${popcorn},${candy},${coke}`);
+
+    //const addButter= new Promise((resolve,reject)=>resolve(`butter`));
+
+    //console.log(`wife:i have ${ticket}`);
+    //console.log('husband: we should go in');
+    //console.log('wife: no i am hungry');
     
-    let popcorn=await getPopcorn;
+    //let popcorn=await getPopcorn;
 
-    console.log(`husband: i got some ${popcorn}`);
-    console.log('husband: we should go in');
-    console.log('wife: i need butter on my popcorn');
+    //console.log(`husband: i got some ${popcorn}`);
+    //console.log('husband: we should go in');
+    //console.log('wife: i need butter on my popcorn');
 
-    let butter = await addButter;
+    //let butter = await addButter;
     
-    console.log(`husband: i got some ${butter} on popcorn`);
-    console.log(`husband: anything else darling`);
-    console.log(`wife:lets go we are getting late`);
-    console.log(`husband: thank you for the reminder *grins* `);
+    //console.log(`husband: i got some ${butter} on popcorn`);
+    //console.log(`husband: anything else darling`);
+    //console.log(`wife:lets go we are getting late`);
+    //console.log(`husband: thank you for the reminder *grins* `);
     
     return ticket;
     
-
-
 }
 
 preMovie().then((m)=>console.log(`person3: shows ${m}`));
