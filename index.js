1e02a69318fe56c099403d0957aa05c0cc698e72
NikitaThakare312 <nikitathakare312@gmail.com>
2023-12-17 19:44:00 +0530

    15th assignment: using try and catch

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ console.log('person2:shows ticket');
 
 const preMovie=async()=>{
     const promiseWifeBringingTicks=new Promise((resolve,reject)=>{
-        setTimeout(()=>resolve('ticket'),3000);
+        setTimeout(()=>reject('ticket'),3000);
     });
 
-    const getPopcorn = new Promise((resolve,reject)=>resolve(`popcorn`));
+    let ticket
+    try{
+        ticket=await promiseWifeBringingTicks;
+    }catch(e){
+        ticket='sad face';
+    }
 
-    const getCandy= new Promise((resolve,reject)=> resolve(`candy`));
+    //const getPopcorn = new Promise((resolve,reject)=>resolve(`popcorn`));
 
-    const getCoke = new Promise((resolve,reject)=>resolve(`coke`));
+    //const getCandy= new Promise((resolve,reject)=> resolve(`candy`));
+
+    //const getCoke = new Promise((resolve,reject)=>resolve(`coke`));
 
 
-    let ticket=await promiseWifeBringingTicks;
+    //let ticket=await promiseWifeBringingTicks;
 
-    let [popcorn,candy,coke]=await Promise.all([getPopcorn,getCandy,getCoke])
+    //let [popcorn,candy,coke]=await Promise.all([getPopcorn,getCandy,getCoke])
 
-    console.log(`${popcorn},${candy},${coke}`);
+    //console.log(`${popcorn},${candy},${coke}`);
 
     //const addButter= new Promise((resolve,reject)=>resolve(`butter`));
 
